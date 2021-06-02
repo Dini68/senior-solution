c24fa93848d9ca0a80dc4116e14eb6d5de2ced49
Dini68 <kdini68@gmail.com>
2021-06-02 20:09:02 +0200

    meetingrooms menü

---
+++ meetingrooms-solution/src/main/java/meetingrooms/MeetingRoom.java
@@ -0,0 +1,67 @@
+package meetingrooms;
+
+public class MeetingRoom {
+
+    private long id;
+    private String name;
+    private int width;
+    private int length;
+
+    public MeetingRoom() {
+    }
+
+    public MeetingRoom(long id, String name, int width, int length) {
+        this.id = id;
+        this.name = name;
+        this.width = width;
+        this.length = length;
+    }
+
+    public MeetingRoom(String name, int width, int length) {
+        this.name = name;
+        this.width = width;
+        this.length = length;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return "MeetingRoom{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", width=" + width +
+                ", length=" + length +
+                '}';
+    }
+}
